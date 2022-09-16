bf1795996af0c68ecdfc4ad38b389234ce0b56d6
elielterman <eelterma@lsoc.org>
2022-09-16 08:57:29 -0500

    added test files

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,19 +6,17 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
+                    item.quality -= 1
             else:
                 if item.quality < 50:
                     item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
+                        if item.quality < 50:
+                            if item.sell_in < 6:
+                                item.quality += 2
+                            elif item.sell_in < 11:
                                 item.quality += 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
