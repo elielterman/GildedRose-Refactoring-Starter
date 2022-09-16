ef5cf2c0d2de9ce806b02b2a72d5256c6983a33b
elielterman <eelterma@lsoc.org>
2022-09-16 09:27:34 -0500

    new commit

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,6 +4,7 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
@@ -21,11 +22,10 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality -= 1
+                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality -= 1
                     else:
                         item.quality = 0
                 else:
